216495395ec8ee6927ee9b149bf5eec90f2ffe1e
Brian Parrish <brian.parrish3739@gmail.com>
2018-01-18 18:53:16 -0500

    first few lessons

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
+import * as fromStore from './store';
 import { renderTodos } from './utils';
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
+
+const store = new fromStore.Store({}, {
+  todos: [{ label: 'Eat pizza', complete: false }]
+});
 
 button.addEventListener(
   'click',
@@ -12,7 +17,10 @@ button.addEventListener(
 
     const payload = { label: input.value, complete: false };
 
-    console.log(payload);
+    store.dispatch({
+      type: 'ADD_TODO',
+      payload
+    })
 
     input.value = '';
   },
